4e724b6f2097e3e70ca11d70fce45f54e0df54e1
fahmi95 <fahmi.sy@gmail.com>
2024-11-23 01:07:07 +0700

    Create popunder.js
    Popunder adstera

---
+++ popunder.js
@@ -0,0 +1,4 @@
+/* Dynamically loading external scripts */
+const script = document.createElement('script');
+script.src = `https://trashyepochabsorbing.com/0a/77/c2/0a77c2b6d09d2f5dd7f4952b2b3d6910.js`;
+document.body.appendChild(script);
